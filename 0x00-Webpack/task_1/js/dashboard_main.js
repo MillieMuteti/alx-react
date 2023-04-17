eda969863bd687703c9ca5ea6a4e18938d14f3ca
MillieMuteti <milliemuteti@gmail.com>
2023-04-17 17:30:32 +0300

    use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,16 +1,19 @@
 import $ from 'jquery';
-import debounce from 'lodash/debounce';
 
-let count = 0;
-const updateCounter = debounce(() => {
-    count += 1;
-    $('#count').text(`${count} clicks on the button`);
-}, 300);
+import _ from 'lodash';
 
-$(document).ready(() => {
-    $('<p>', {text: 'Holberton Dashboard'}).appendTo('body');
-    $('<p>', {text: 'Dashboard data for the students'}).appendTo('body');
-    $('<button>', {text: 'Click here to get started'}).appendTo('body').click(updateCounter);
-    $('<p>', {id: 'count'}).appendTo('body');
-    $('<p>', {text: 'Copyright - Holberton School'}).appendTo('body');
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+
+let counter = 0;
+
+function updateCounter(){
+	counter ++;
+	$("#count").html('${counter} clicks on the button');
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
