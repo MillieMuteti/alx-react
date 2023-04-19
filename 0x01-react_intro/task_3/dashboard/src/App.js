c0fa000dad83ed9b860f1d9cf9cfa9a919e8a8b0
MillieMuteti <milliemuteti@gmail.com>
2023-04-19 22:56:31 +0300

    basic tests with four tests

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,37 @@
+import logo from './holberton-logo.png';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" />
+
+        <button type="button">OK</button>
+        </form>
+      </div>
+
+     
+      <div className="App-footer">
+        <p>{getFooterCopy(true)}</p>
+        <p>{getFullYear()}</p>
+      </div>
+    
+
+    </div>
+  );
+}
+
+export default App;
+
